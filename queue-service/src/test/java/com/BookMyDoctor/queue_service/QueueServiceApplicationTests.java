189d41971c003c4de300e3f7e66f19bef7fb0eb0
aloui_houssem <aloui9861@gmail.com>
2025-07-14 19:40:22 +0200

    set up project structure

---
+++ queue-service/src/test/java/com/BookMyDoctor/queue_service/QueueServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BookMyDoctor.queue_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueueServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
